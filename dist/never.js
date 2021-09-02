add74a1c4dca9f0644d18c169445e7af801bc86b
qzyqzy <15757745974@163.com>
2021-09-02 22:31:12 +0800

    ts 基础类型

--- dist/never.js
+++ dist/never.js
@@ -1,5 +1,13 @@
 "use strict";
-var add = function () {
-    while (true) { }
-};
-add();
+// 必须传入对象  x  y
+function add(val) {
+    console.log(val.x + val.y);
+}
+function mul(val) {
+    console.log(val.x * val.y);
+}
+var c = { x: 4, y: 5 };
+var b = { x: 4, y: 5, z: 6 };
+add(c);
+mul(b); //20
+add({ x: 4, y: 5, z: 6 });
--- 基本类型/never.ts
+++ 基本类型/never.ts
@@ -1,5 +1,18 @@
-let add = function (): never {
-  while (true) {}
-};
+// 接口：行为和动作的规范，对批量方法进行约束
+interface data {
+  x: number;
+  y: number;
+}
 
-add();
+// 必须传入对象  x  y
+function add(val: data): void {
+  console.log(val.x + val.y);
+}
+function mul(val: data): void {
+  console.log(val.x * val.y);
+}
+let c = { x: 4, y: 5 };
+let b = { x: 4, y: 5, z: 6 };
+add(c);
+mul(b); //20
+add({ x: 4, y: 5, z: 6 });
